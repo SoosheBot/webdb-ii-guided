00c33cb6b2a01eef1ea54fc6815f469181bfbdc2
sooshe <sooshe@sooshebot.com>
2019-12-10 11:45:03 -0600

    knex make table

--- knexfile.js
+++ knexfile.js
@@ -4,46 +4,54 @@
 
 //each key on the exported object has the configuration for a db environment, (dev, production, testing, etc)
 
+
+
 module.exports = {
 
   development: {
     client: 'sqlite3',
-    useNullAsDefault:true,
+    useNullAsDefault:true, //only needed when using sqlite3
     connection: {
-      filename: './data/produce/db3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './data/produce/db3' //where is the database file? this tells you
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations' //will be created automatically, unless you write this, then it tells it what to look at/teaches it how to connect
+    },
+
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
+  //removed these things that came after development because we really don't need them -- can always do a knex init somewhere and grab this stuff if needed
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+
---
+++ migrations/20191210114343_addVeggiesTable.js
@@ -0,0 +1,11 @@
+//a migration file describes the changes to apply to the db
+//the up function describes what will be done
+//the down function describes how to revert the changes
+
+exports.up = function(knex) {
+  
+};
+
+exports.down = function(knex) {
+  
+};
