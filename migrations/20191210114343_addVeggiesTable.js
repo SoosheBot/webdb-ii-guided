1ec83afa2cfdea712f8b0acfbc30fa6d4dc98cdd
sooshe <sooshe@sooshebot.com>
2019-12-10 12:20:17 -0600

    configure addveggietables

--- migrations/20191210114343_addVeggiesTable.js
+++ migrations/20191210114343_addVeggiesTable.js
@@ -3,9 +3,21 @@
 //the down function describes how to revert the changes
 
 exports.up = function(knex) {
+    // ALWAYS RETURN THE CALL TO KNEX.SCHEMA
+    return knex.schema.createTable('veggies', tbl => {
+        // primary key, called 'id', which is usually an integer that auto-increments -- to write it so it does this right out hte box, do the following:
+        tbl.increments('id');
+        // .unique() is a constraint -- rules we define to protect against invalid data
+        // .notNullable() -- another constraint that requires we add some text in this field or it gives an error
+        tbl.string('name', 255)
+        .unique()
+        .notNullable()
+        .index(); // .index() makes searching for vegetables using this column faster -- you can add it for one column, two columns (if you need to have two things linked -- like name and phone number or name, email, for example)
+        tbl.boolean('like_it').defaultTo(false) // true is 1, false is 0?, can put a 1/0 or a true/false without quotes
+    });
   
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('veggies');
 };
